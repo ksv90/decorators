108911a5ea0070bff65537b35c8543d567d5109e
sergey.krasnov <sergey.krasnov@truelab.games>
2024-07-15 13:43:39 +0300

    fix: broadcaster created

---
+++ src/broadcaster/broadcaster.ts
@@ -0,0 +1,51 @@
+import { ClassDecorator, Constructor } from '../types';
+import { IBroadcaster, Subscriber } from './types';
+
+export const Broadcaster = <TEvents extends object, TConstructor extends Constructor>(
+  name: string,
+): ClassDecorator<TConstructor, TConstructor & Constructor<IBroadcaster<TEvents>>> => {
+  return (Target) => {
+    abstract class BroadcasterMixin extends Target implements IBroadcaster<TEvents> {
+      readonly #subscriberMap = new Map<keyof TEvents, Set<Subscriber>>();
+
+      readonly #broadcastChannel = new BroadcastChannel(name);
+
+      // ! open issue https://github.com/microsoft/TypeScript/issues/37142
+      // constructor(...args: ConstructorParameters<TConstructor>)
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      constructor(...args: any) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        super(...args);
+
+        this.#broadcastChannel.addEventListener('message', (ctx: MessageEvent<{ event: keyof TEvents; data: unknown }>) => {
+          const { event, data } = ctx.data;
+          const subscribers = this.#subscriberMap.get(event);
+          subscribers?.forEach((subscriber) => {
+            subscriber(data);
+          });
+        });
+      }
+
+      subscribe<TEvent extends keyof TEvents>(event: TEvent, subscriber: Subscriber<TEvents[TEvent]>): void {
+        let subscribers = this.#subscriberMap.get(event);
+        if (!subscribers) {
+          subscribers = new Set<Subscriber>();
+          this.#subscriberMap.set(event, subscribers);
+        }
+        subscribers.add(subscriber);
+      }
+
+      unsubscribe<TEvent extends keyof TEvents>(event: TEvent, subscriber: Subscriber<TEvents[TEvent]>): void {
+        const listeners = this.#subscriberMap.get(event);
+        listeners?.delete(subscriber);
+      }
+
+      publish<TEvent extends keyof TEvents, TData extends TEvents[TEvent]>(event: TEvent, data: TData): void {
+        this.#broadcastChannel.postMessage({ event, data });
+      }
+    }
+
+    return BroadcasterMixin;
+  };
+};
